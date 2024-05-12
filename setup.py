331e93482eefbe6b0ae41a8c8bb00e8a1368d263
jhahncs <jhahncs@gmail.com>
2024-05-13 01:10:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup, find_packages
-
+from glob import glob
+from os.path import basename, splitext
+from setuptools import find_packages, setup
 
 install_requires = [
     'pandas',
@@ -12,6 +13,8 @@ setup (
     name             = 'toy', 
     packages=find_packages(where='src'),
     version          = '1.0.0',
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     install_requires = install_requires,
     author           = 'jhahn',
     author_email     = 'jhahncs@gmail.com',
